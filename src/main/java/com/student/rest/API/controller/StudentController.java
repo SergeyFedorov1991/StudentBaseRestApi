afc5cf3f7bd472f4a5d2cd469cbde98d6849cfba
SergeyFedorov1991 <serstalk@gmail.com>
2020-08-22 10:47:44 +0300

    init

--- src/main/java/com/student/rest/API/controller/StudentController.java
+++ src/main/java/com/student/rest/API/controller/StudentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RestController
 @RequestMapping("/students")
 public class StudentController {
